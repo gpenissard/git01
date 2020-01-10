bab8238315f844a97757b1057ec865717d1b6f09
Gpenissard <gpenissard@gmail.com>
2020-01-09 20:56:42 -0500

    updated page title

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Git01</title>
+    <title>Home page of Git03</title>
 </head>
 <body>
     
